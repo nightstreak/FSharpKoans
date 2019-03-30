0889454f6f02396e086a8c290524e135d45a9d3c
nightstreak <prumo101@gmail.com>
2019-03-30 12:20:51 +0200

    Did everything I could

--- FSharpKoans/AboutLists.fs
+++ FSharpKoans/AboutLists.fs
@@ -16,8 +16,8 @@ module ``12: I Have Here In My Hand A List`` =
     let ``02 Creating a list (Syntax 2).`` () =
         let myList = "apple"::"grape"::"pear"::"biscuit"::[]
         let myOtherList = "orange"::"lemon"::"princess"::[ "queen" ]
-        let myNextList = __::__::__ // you may use [ and ] symbols on this line.
-        let myLastList = __::__::__ // DO NOT use [ or ] symbols on this line!
+        let myNextList = "lily"::"sunflower"::["daisy";"carrot"] // you may use [ and ] symbols on this line.
+        let myLastList = "naartjie"::"raisin"::myList // DO NOT use [ or ] symbols on this line!
         myList |> should equal [ "apple"; "grape"; "pear"; "biscuit" ]
         myOtherList |> should equal [ "orange"; "lemon"; "princess"; "queen" ]
         myNextList |> should equal ["lily"; "sunflower"; "daisy"; "carrot"]
@@ -27,7 +27,7 @@ module ``12: I Have Here In My Hand A List`` =
     let ``03 Creating a list (via concatenation).`` () =
         let a = [902; 10]
         let b = [3; 13; 37]
-        let result = __ @ __
+        let result = a @ b
         result |> should equal [902; 10; 3; 13; 37]
 
     [<Test>]
@@ -35,42 +35,42 @@ module ``12: I Have Here In My Hand A List`` =
         let first = [ "grape"; "peach" ]
         let second = "pear" :: first
         let third = "apple" :: second
-        third |> should equal __
-        second |> should equal __
-        first |> should equal __
+        third |> should equal ["apple";"pear";"grape";"peach"]
+        second |> should equal ["pear";"grape";"peach"]
+        first |> should equal ["grape"; "peach"]
 
     [<Test>]
     let ``05 Pattern-matching a list (Part 1).`` () =
         let fruits = ["apple"; "peach"; "orange"; "watermelon"; "pineapple"; "tomato"]
         let a::_ = fruits
-        a |> should equal __
+        a |> should equal "apple"
 
     [<Test>]
     let ``06 Pattern-matching a list (Part 2).`` () =
         let fruits = ["apple"; "peach"; "orange"; "watermelon"; "pineapple"; "tomato"]
         let b::c::_ = fruits
-        b |> should equal __
-        c |> should equal __
+        b |> should equal "apple"
+        c |> should equal "peach"
 
     [<Test>]
     let ``07 Pattern-matching a list (Part 3).`` () =
         let fruits = ["apple"; "peach"; "orange"; "watermelon"; "pineapple"; "tomato"]
         let _::d::e = fruits
-        d |> should equal __
-        e |> should equal __
+        d |> should equal "pineapple"
+        e |> should equal "tomato"
 
     [<Test>]
     let ``08 Pattern-matching a list (Part 4).`` () =
         let fruits = ["apple"; "peach"; "orange"; "watermelon"; "pineapple"; "tomato"]
         let f::_::_::g::_ = fruits
-        f |> should equal __
-        g |> should equal __
+        f |> should equal "apple"
+        g |> should equal "pineapple"
 
     [<Test>]
     let ``09 Pattern-matching a list (Part 5).`` () =
         let fruits = ["apple"; "peach"; "orange"; "watermelon"; "pineapple"; "tomato"]
         let _::_::_::h = fruits
-        h |> should equal __
+        h |> should equal ["apple"; "peach"; "orange"; "watermelon"; "pineapple";"tomato"]
 
     [<Test>]
     let ``10 Pattern-matching a list (Part 6).`` () =
--- FSharpKoans/AboutOptions.fs
+++ FSharpKoans/AboutOptions.fs
@@ -58,8 +58,8 @@ module ``11: Exploring types, options, and results`` =
 
     [<Test>]
     let ``03 Basic Option example`` () =
-        getSurname "Taylor Swift" |> should equal Some 
-        getSurname "Eminem" |> should equal Some
+        getSurname "Taylor Swift" |> should equal (Some "Swift") 
+        getSurname "Eminem" |> should equal (None)
 
     // the System.Int32.TryParse, System.Double.TryParse, etc functions return
     // a tuple of bool * XYZ, where XYZ is the converted value.
@@ -67,7 +67,8 @@ module ``11: Exploring types, options, and results`` =
     let ``04 Parsing a string safely`` () =
         let parse (s:string) =
             match System.Int32.TryParse s with
-            | _ -> __ // <-- fill in the match cases
+            | true, v -> Some v // <-- fill in the match cases
+            |_-> None
         parse "25" |> should equal (Some 25)
         parse "48" |> should equal (Some 48)
         parse "wut" |> should equal None
@@ -76,7 +77,8 @@ module ``11: Exploring types, options, and results`` =
     let ``05 Remapping Option values`` () =
       let f n =
          match getSurname n with
-         | _ -> __ // <-- write a bunch of good match cases
+         | Some n -> n // <-- write a bunch of good match cases
+         | None -> "[no surname]"
       f "Anubis" |> should equal "[no surname]"
       f "Niccolo Machiavelli" |> should equal "Machiavelli"
       f "Mara Jade" |> should equal "Jade"
@@ -90,10 +92,10 @@ module ``11: Exploring types, options, and results`` =
         let f n m =
             match n<0.0, m=0.0 with
             | true, _ -> Error NegativeNumberSupplied
-            | _, true -> __
+            | _, true -> Error DivisionByZero
             | _ ->
                 // 'sqrt' is the square-root function
                 __ (sqrt n / m)
-        f -6.0 2.5 |> shouldEqual __
+        f -6.0 2.5 |> shouldEqual (Error NegativeNumberSupplied)
         f 144.0 2.0 |> shouldEqual (Ok 6.0)
         f 7.3 0.0 |> shouldEqual (Error DivisionByZero)
